956800b8539c65c61232595440b806163317cff0
soncrayon <arnold.curtis@gmail.com>
2021-07-03 14:51:54 -0400

    provide CRUD methods to teacher and student controllers

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,18 +1,31 @@
 class StudentsController < ApplicationController
 
+    before_action :set_student, only: [:show, :edit, :delete]
+    
     def index
+        render :json => Student.all
     end
 
     def create
+        render :json => Student.new(params)
     end
 
     def show
+        render :json => @student
     end
 
     def edit
+        render :json => @student.update(params)
     end
 
     def delete
+        @student.destroy
+    end
+
+    private
+
+    def set_student
+        @student = Student.find(params[:id])
     end
     
 end
--- app/controllers/teachers_controller.rb
+++ app/controllers/teachers_controller.rb
@@ -1,18 +1,31 @@
 class TeachersController < ApplicationController
 
+    before_action :set_teacher, only: [:show, :edit, :delete]
+    
     def index
+        render :json => Teacher.all
     end
 
     def create
+        render :json => Teacher.new(params)
     end
 
     def show
+        render :json => @teacher
     end
 
     def edit
+        render :json => @teacher.update(params)
     end
 
     def delete
+        @teacher.destroy
+    end
+
+    private
+
+    def set_teacher
+        @teacher = Teacher.find(params[:id])
     end
     
 end
